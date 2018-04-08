957919ba2f4caba583f414df6fea4a42990e6300
Matt Jenkins <matt@majenko.co.uk>
2018-04-08 23:17:32 +0100

    Improved example

--- examples/TMCTest/TMCTest.ino
+++ examples/TMCTest/TMCTest.ino
@@ -1,3 +1,15 @@
+/* Test of the TMC support
+ *
+ * Send the string ":AINx" where x is 0-5 to
+ * request the analog input x value.  Value is
+ * returned in the next read.
+ *
+ * Send the string ":TIME" and the millis() value
+ * is returned in the next read.
+ *
+ * Unknown commands are dumped to the CDC/ACM port.
+ */
+ 
 #include <USB_TMC.h>
 
 USBFS usbDevice;
@@ -6,7 +18,26 @@ USB_TMC TMC;
 CDCACM uSerial;
 
 void gotMessage(uint8_t *msg, uint32_t len) {
-    for (int i = 0; i < len; i++) {
+    if (msg[0] == ':') {
+        if (!strncmp((char *)msg, ":AIN", 4)) {
+            uint32_t chan = strtoul((char *)msg + 4, NULL, 10);
+            if (chan < 6) {
+                char temp[6];
+                sprintf(temp, "%d\n", analogRead(chan));
+                TMC.send((uint8_t *)temp, strlen(temp), TMC_REPLACE);
+            }
+            return;
+        }
+    
+        if (!strncmp((char *)msg, ":TIME", 5)) {
+            char temp[20];
+            sprintf(temp, "%lu\n", millis());
+            TMC.send((uint8_t *)temp, strlen(temp), TMC_REPLACE);
+            return;
+        }
+    }
+    
+    for (size_t i = 0; i < len; i++) {
         uSerial.write(msg[i]);
     }
 }
@@ -19,5 +50,4 @@ void setup() {
 }
 
 void loop() {
-    
 }
